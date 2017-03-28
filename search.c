f26e83d31e5903a75c886c2fdd0bfbd3d290047d
darksystem <diogo.rey@hotmail.com>
2017-03-28 11:27:34 +0100

    Read folders recursively but dont read files idk why

--- search.c
+++ search.c
@@ -18,9 +18,53 @@ int main(int argc, char *argv[]) {
 }
 
 
+int checkType(char * d_name, char type){
+	struct stat stat_buf;
+
+	if(lstat(d_name, &stat_buf) == -1){
+		fprintf(stderr, "Could not read status from current directory");
+		exit(-1);
+	}
+
+	switch (type){
+		case 'c': //character (unbuffered) special
+		if (S_ISCHR(stat_buf.st_mode)) return 1;
+		break;
+		case 'd': //directory
+		if (S_ISDIR(stat_buf.st_mode)) return 1;
+		break;
+
+		case 'p': // named pipe (FIFO)
+		if (S_ISFIFO(stat_buf.st_mode)) return 1;
+		break;
+
+		case 'f': // regular file
+		if (S_ISREG(stat_buf.st_mode)) return 1;
+		break;
+
+		case 'l': //symbolic link;
+		if (S_ISLNK(stat_buf.st_mode)) return 1;
+		break;
+
+		case 's': //socket
+		if (S_ISSOCK(stat_buf.st_mode)) return 1;
+		break;
+		case 'D': //door (Solaris)
+		if (S_ISBLK(stat_buf.st_mode)) return 1;
+		break;
+
+		default:
+		break;
+	}
+	return 0;
+
+}
+
+
 void listFilesFromDir(char* workingDir) {
 
-	DIR *dir, *sDir;
+	DIR *dir;
+	//, *sDir;
 	struct dirent *entry;
 	char * d_name;
 	//char * subDirName;
@@ -30,35 +74,42 @@ void listFilesFromDir(char* workingDir) {
 		fprintf(stderr, "Cannot open directory '%s': %s\n", workingDir, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	
+
+	strcpy(subDirName,workingDir);
+
 	while (1) {
 
+
 		if(!(entry = readdir(dir)))
-			break;
+		break;
 
 		d_name = entry->d_name;
 
 		if (!strcmp(d_name, ".") || !strcmp(d_name, ".."))
-			continue;
+		continue;
 		else {
 			printf("%s/%s\n", workingDir, d_name);
-			if ((sDir = opendir(d_name))){
-				strcpy(subDirName,workingDir);
-				strcat(subDirName,"/");
-				strcat(subDirName,d_name);
-				//printf("ZZZZZ\n%s\n\n",subDirName);
-				listFilesFromSubDir(subDirName);
-				closedir(sDir);
+			strcat(subDirName,"/");
+			strcat(subDirName,d_name);
+			strcat(subDirName,"\0");
+
+			if ( checkType(subDirName, 'd') == 1){
+				listFilesFromDir(subDirName);
+
 			}
+
 		}
 	}
 	closedir(dir);
+
+	exit(0);
 }
 
 void listFilesFromSubDir(char* workingDir) {
 
-	printf("\n\nWORKINGDIR: %s", workingDir);
-	DIR *dir, *sDir;
+	printf("\n\nWORKINGDIR: %s \n", workingDir);
+	DIR *dir;
+	//, *sDir;
 	struct dirent *entry;
 	char * d_name;
 	char subDirName[] = "";
@@ -67,26 +118,30 @@ void listFilesFromSubDir(char* workingDir) {
 		fprintf(stderr, "Cannot open directory '%s': %s\n", workingDir, strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	
+
+	strcpy(subDirName,workingDir);
+
 	while (1) {
 
 		if(!(entry = readdir(dir)))
-			break;
+		break;
 
 		d_name = entry->d_name;
 
 		if (!strcmp(d_name, ".") || !strcmp(d_name, ".."))
-			continue;
+		continue;
 		else {
+			//printf("\nEntrei aqui2\n");
 			printf("%s/%s\n", workingDir, d_name);
-			if ((sDir = opendir(d_name))){
-				strcpy(subDirName,workingDir);
-				strcat(subDirName,"/");
-				strcat(subDirName,d_name);
-				listFilesFromSubDir(subDirName);	
-				closedir(sDir);
+			strcat(subDirName,"/");
+			strcat(subDirName,d_name);
+			strcat(subDirName,"\0");
+			if ( checkType(subDirName, 'd') ==1 ){
+				printf ("\n\n\n");
+				listFilesFromSubDir(subDirName);
 			}
 		}
 	}
 	closedir(dir);
+	exit(0);
 }
